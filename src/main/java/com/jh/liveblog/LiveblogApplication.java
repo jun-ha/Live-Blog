f5c16d16c84f2e872751c27a2e7d94124e514c88
Junha <yy4124@gmail.com>
2025-01-21 15:08:31 +0900

    first commit

---
+++ src/main/java/com/jh/liveblog/LiveblogApplication.java
@@ -0,0 +1,13 @@
+package com.jh.liveblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jh/liveblog/LiveblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.liveblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
